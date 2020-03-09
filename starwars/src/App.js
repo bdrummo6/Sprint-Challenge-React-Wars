76d5153281e34e7bd659c7734c1030d3bf7bcc49
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-03-09 01:02:12 -0400

    Update App.js
    Finally determined how to get my data to display, I just had to set my data to results and that fixed that problem.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,25 +3,25 @@ import axios from 'axios';
 import StarWars from "./components/StarWars";
 import './App.css';
 
-const App = (props) => {
+const App = () => {
    // Try to think through what state you'll need for this app before starting. Then build out
    // the state properties here.
-   const [data, setData] = useState({ starwarsChars: [] });
-
+   const [data, setData] = useState([]);
+   const [error, setError] = useState();
+   
    useEffect(() => {
-      const fetchData = () => {
-         axios
-            .get(`https://swapi.co/api/people`)
-            .then(response => setData(response.data));
-      };
-
-      fetchData();
+      axios.get(`https://swapi.co/api/people`)
+         .then(response => {
+            console.log(response.data);
+            setData(response.data.results);
+         })
+         .catch(error => setError(error.response));
    }, []);
 
    return (
       <div className="App">
          <h1 className="Header">React Wars</h1>
-         <StarWars characterList={data.starwarsChars} />
+         <StarWars characterList={data} />
       </div>
    );
 };
